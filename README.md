377f8fd6e0d6f92ed44a3780b6d8685ee32d77dd
GideonNaah <gideonnaah19@gmail.com>
2021-12-15 10:59:18 +0000

    added all files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Userswebservices
+
+This web service is used to manage users. it allows to create and fetch all users.
+
+## Tools
+express
+morgan
---
+++ data/users.js
@@ -0,0 +1,7 @@
+let users =[
+    {fname:"Gibson", lname: "Anderson", dod: "14/12/2000", school:'Umat'},
+    {fname:"giddy", lname: "Basil", dod: "04/11/2000", school:'Knust'},
+    {fname:"maxwell", lname: "haddi", dod: "03/04/1999", school:'Wa UDS'},
+]
+
+module.exports=users;
